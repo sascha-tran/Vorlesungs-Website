a3c3475624186df881fd5d28571bd534066e7f63
sascha <sascha.tran@stud.unibas.ch>
2021-11-28 13:43:52 +0100

    add Sascha's Zoomlinks, mainly nine-eleven

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-var base = ["software-engineering", "scientific-computing"];
+var base = ["software-engineering", "scientific-computing", "nine-eleven"];
   var all = [
     "software-engineering",
     "scientific-computing",
     "discrete-mathematics",
     "catch21",
+    "nine-eleven",
   ];
 
   var vEdi = ["discrete-mathematics", "catch21"];
 
-  var vSascha = ["software-engineering", "scientific-computing"];
+  var vSascha = ["software-engineering", "scientific-computing", "nine-eleven"];
 
   function checkbox() {
     if (document.getElementById("edi").checked) {
@@ -35,4 +36,4 @@ var base = ["software-engineering", "scientific-computing"];
     all.forEach((element) => {
         document.getElementById(element).style = "display: none;";
       });
-    }
+  }
